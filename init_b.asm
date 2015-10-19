077dcfd75d4fdd7b540bbafb307ac593a6d69eb5
maketheking <maketheking@gmail.com>
2015-10-19 20:10:40 +0200

    Inicio

---
+++ init_b.asm
@@ -0,0 +1,67 @@
+.text
+#        ENTRY                  /*  mark the first instruction to call */
+.global start
+start:
+.arm /*indicates that we are using the ARM instruction set */
+#------standard initial code
+# --- Setup interrupt / exception vectors
+      B       Reset_Handler
+/* In this version we do not use the following handlers */
+#######################################################################################################
+#-----------Undefined_Handler:
+#      B       Undefined_Handler
+#----------SWI_Handler:
+#      B       SWI_Handler
+#----------Prefetch_Handler:
+#      B       Prefetch_Handler
+#----------Abort_Handler:
+#      B       Abort_Handler
+#         NOP      /* Reserved vector */
+#----------IRQ_Handler:
+#      B       IRQ_Handler
+#----------FIQ_Handler:
+#      B       FIQ_Handler
+#######################################################################################################
+# Reset Handler:
+# the processor starts executing this code after system reset
+#######################################################################################################
+Reset_Handler:
+#
+        MOV     sp, #0x4000      /*  set up stack pointer (r13) */
+#
+#  USING A .C FUNCTION
+#
+# FUNCTION CALL the parameters are stored in r0 and r1
+# If there are 4 or less parameters when calling a C function the compiler assumes that they have
+# been stored in r0-r3. If there are more parameters you have to store them in the data stack using the stack pointer
+# function __c_copy is in copy.c
+        LDR     r0, =cuadricula  /*  puntero a la cuadricula */
+
+.extern     sudoku9x9
+        ldr         r5, = sudoku9x9
+        mov         lr, pc
+        bx          r5
+
+stop:
+        B       stop        /*  end of program */
+
+#################################################################################################################
+.data
+.ltorg /*guarantees the alignment*/
+.align 5
+
+# huecos para cuadrar
+cuadricula:
+     /* 9 filas de 16 entradas para facilitar la visualizacion y 16 bits por celda */
+    .hword   0x9800,0x6800,0x0000,0x0000,0x0000,0x0000,0x7800,0x0000,0x8800,0,0,0,0,0,0,0
+    .hword   0x8800,0x0000,0x0000,0x0000,0x0000,0x4800,0x3800,0x0000,0x0000,0,0,0,0,0,0,0
+    .hword   0x1800,0x0000,0x0000,0x5800,0x0000,0x0000,0x0000,0x0000,0x0000,0,0,0,0,0,0,0
+    .hword   0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x1800,0x7800,0x6800,0,0,0,0,0,0,0
+    .hword   0x2800,0x0000,0x0000,0x0000,0x9800,0x3800,0x0000,0x0000,0x5800,0,0,0,0,0,0,0
+    .hword   0x7800,0x0000,0x8800,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0,0,0,0,0,0,0
+    .hword   0x0000,0x0000,0x7800,0x0000,0x3800,0x2800,0x0000,0x4800,0x0000,0,0,0,0,0,0,0
+    .hword   0x3800,0x8800,0x2800,0x1800,0x0000,0x5800,0x6800,0x0000,0x0000,0,0,0,0,0,0,0
+    .hword   0x0000,0x4800,0x1800,0x0000,0x0000,0x9800,0x5800,0x2800,0x0000,0,0,0,0,0,0,0
+
+.end
+#        END
---
+++ sudoku_2015.c
@@ -0,0 +1,172 @@
+#include <inttypes.h>
+
+// Tamaños de la cuadricula
+// Se utilizan 16 columnas para facilitar la visualización
+enum {
+	NUM_FILAS = 9, NUM_COLUMNAS = 16, PADDING = 7
+};
+
+// Definiciones para valores muy utilizados
+enum {
+	FALSE = 0, TRUE = 1
+};
+
+typedef uint16_t CELDA;
+// La información de cada celda está codificada en 16 bits
+// con el siguiente formato (empezando en el bit más significativo):
+// 4 MSB VALOR
+// 1 bit PISTA
+// 1 bit ERROR
+// 1 bit no usado
+// 9 LSB CANDIDATOS
+
+inline void celda_poner_valor(CELDA *celdaptr, uint8_t val) {
+	*celdaptr = (*celdaptr & 0x0FFF) | ((val & 0x000F) << 12);
+}
+
+inline uint8_t celda_leer_valor(CELDA celda) {
+	return (celda >> 12);
+}
+
+// funcion a implementar en ARM
+extern int
+sudoku_candidatos_arm(CELDA cuadricula[NUM_FILAS][NUM_COLUMNAS]);
+
+// funcion a implementar en Thumb
+extern int
+sudoku_candidatos_thumb(CELDA cuadricula[NUM_FILAS][NUM_COLUMNAS]);
+
+////////////////////////////////////////////////////////////////////////////////
+// dada una determinada celda encuentra los posibles valores candidatos
+// y guarda el mapa de bits en la celda
+// retorna false si la celda esta vacia, true si contiene un valor
+int sudoku_candidatos_c(CELDA cuadricula[NUM_FILAS][NUM_COLUMNAS], uint8_t fila,
+		uint8_t columna) {
+
+	uint8_t i, j;
+
+	//si NO es uno de los numeros fijados inicialmente (pista = 0)
+	if ((cuadricula[fila][columna] & 0x0800) == 0) {
+
+		//si tiene un valor erroneo, o no tiene valor
+		if ((cuadricula[fila][columna] & 0x0400) == 0
+				|| (cuadricula[fila][columna] & 0xF000) != 0) {
+
+			//TODO: Tratar error, borrando bit de error y el valor de la celda
+
+			// iniciar candidatos
+			cuadricula[fila][columna] |= 0x01FF;
+
+			//precalcular el numero de columnas, evitando el padding
+			uint8_t limiteColumna = NUM_COLUMNAS - PADDING;
+
+			// recorrer fila recalculando candidatos
+			for (i = 0; i < limiteColumna; i++) {
+
+				//evita comprobar la celda que estamos tratando
+				if (i != columna) {
+
+					//eliminar valores de candidatos
+					//TODO: comprobar si hay error
+					uint8_t valor = celda_leer_valor(cuadricula[fila][i]);
+					if (valor != 0) {
+
+						//pone a cero el bit referente al candidato encontrado
+						cuadricula[fila][columna] &= ~(0x0001 << (valor - 1));
+					}
+				}
+
+			}
+
+			// recorrer columna recalculando candidatos
+			for (i = 0; i < NUM_FILAS; i++) {
+
+				//evita comprobar la celda que estamos tratando
+				if (i != fila) {
+
+					//eliminar valores de candidatos
+					//TODO: comprobar si hay error
+					uint8_t valor = celda_leer_valor(cuadricula[i][columna]);
+					if (valor != 0) {
+
+						//pone a cero el bit referente al candidato encontrado
+						cuadricula[fila][columna] &= ~(0x0001 << (valor - 1));
+					}
+				}
+
+			}
+
+			// recorrer region recalculando candidatos
+			// obtener region
+			uint8_t regionColumna = columna / 3;
+			uint8_t regionFila = fila / 3;
+
+			// calcular punto inicial de la region
+			regionColumna = regionColumna * 3;
+			regionFila = regionFila * 3;
+
+			// recorrer region
+			for (i = regionColumna; i < regionColumna + 3; i++) {
+				for (j = regionFila; j < regionFila + 3; j++) {
+
+					if (i != columna || j != fila) {
+
+						//eliminar valores de candidatos
+						//TODO: comprobar si hay error
+						uint8_t valor = celda_leer_valor(cuadricula[j][i]);
+
+						if (valor != 0) {
+
+							//pone a cero el bit referente al candidato encontrado
+							cuadricula[fila][columna] &= ~(0x0001 << (valor - 1));
+						}
+					}
+				}
+			}
+		}
+	}
+
+// retornar indicando si la celda tiene un valor o esta vacia
+	if ((cuadricula[fila][columna] & 0xF000) != 0)
+		return TRUE;
+	return FALSE;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// recalcula todo el tablero (9x9)
+// retorna el numero de celdas vacias
+int sudoku_recalcular_c(CELDA cuadricula[NUM_FILAS][NUM_COLUMNAS]) {
+
+	uint8_t contador = 0;
+	uint8_t i, j;
+	uint8_t limiteColumna = NUM_COLUMNAS - PADDING;
+
+//para cada fila
+	for (i = 0; i < NUM_FILAS; i++) {
+
+		//para cada columna
+		for (j = 0; j < limiteColumna; j++) {
+
+			//determinar candidatos
+			if (sudoku_candidatos_c(cuadricula, i, j) == FALSE) {
+
+				//actualizar contador de celdas vacias
+				contador++;
+			}
+		}
+	}
+
+//retornar el numero de celdas vacias
+	return contador;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// proceso principal del juego que recibe el tablero,
+// y la señal de ready que indica que se han actualizado fila y columna
+void sudoku9x9(CELDA cuadricula[NUM_FILAS][NUM_COLUMNAS], char *ready) {
+	int celdas_vacias;     //numero de celdas aun vacias
+
+	celdas_vacias = sudoku_recalcular_c(cuadricula);
+
+}
+
